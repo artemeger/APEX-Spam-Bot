553b082ab4a51cd28c498be126e98475ca883976
Artem <ae@hazana.de>
2019-01-18 23:43:59 +0100

    update

--- src/main/java/com/apex/bot/TelegramMessageHandler.java
+++ src/main/java/com/apex/bot/TelegramMessageHandler.java
@@ -1,3 +1,27 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2017 - 2019
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.apex.bot;
 
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.DeleteMessage;
@@ -20,6 +44,7 @@ public class TelegramMessageHandler extends ATelegramBot {
     private static final String PDF= ".pdf";
     private static final String PNG = ".png";
     private static final String JPEG = ".jpeg";
+    private static final String JPG = ".jpg";
     private static final String GIF = ".gif";
 
     public TelegramMessageHandler(String token, String botname) {
@@ -31,12 +56,13 @@ public class TelegramMessageHandler extends ATelegramBot {
         int id = update.getMessage().getMessageId();
         if(update.getMessage().hasDocument()){
             Document doc  = update.getMessage().getDocument();
-            if(!(doc.getMimeType().equals(MP3_MIME) || doc.getMimeType().equals(MP4_MIME) || doc.getMimeType().equals(MPEG_MIME)
-                    || doc.getMimeType().equals(PDF_MIME) || doc.getMimeType().equals(PNG_MIME) || doc.getMimeType().equals(JPEG_MIME)
-                    || doc.getMimeType().equals(GIF_MIME)) &&
-                    !(doc.getFileName().endsWith(MP3) || doc.getFileName().endsWith(MP4) || doc.getFileName().endsWith(MPEG)
-                    || doc.getFileName().endsWith(PDF) || doc.getFileName().endsWith(PNG) || doc.getFileName().endsWith(JPEG)
-                    || doc.getFileName().endsWith(GIF))){
+            String mimeName = doc.getMimeType();
+            String fileName = doc.getFileName();
+            if(!(mimeName.equals(MP3_MIME) || mimeName.equals(MP4_MIME) || mimeName.equals(MPEG_MIME)
+            || mimeName.equals(PDF_MIME) || mimeName.equals(PNG_MIME) || mimeName.equals(JPEG_MIME)
+            || mimeName.equals(GIF_MIME)) || !(fileName.endsWith(MP3) || fileName.endsWith(MP4)
+            || fileName.endsWith(MPEG) || fileName.endsWith(PDF) || fileName.endsWith(PNG)
+            || fileName.endsWith(JPEG) || fileName.endsWith(GIF) || fileName.endsWith(JPG))){
                 log.info(String.valueOf(id));
                 DeleteMessage delete = new DeleteMessage(update.getMessage().getChatId(), id);
                 try {
